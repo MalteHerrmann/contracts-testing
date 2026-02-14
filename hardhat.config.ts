83c251063bcc516103c14295ad305f8b8439ad1d
Malte Herrmann <malteherrmann.mail@gmail.com>
2026-02-15 00:36:45 +0100

    syncro update 2026-02-15

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,9 +7,9 @@ config();
 
 // Check that the required environment variables are set
 if (
-  !process.env.LOCAL_KEYS
-  || !process.env.TESTNET_KEYS
-  || !process.env.MAINNET_KEYS
+  !process.env.LOCAL_KEYS ||
+  !process.env.TESTNET_KEYS ||
+  !process.env.MAINNET_KEYS
 ) {
   throw new Error("some required keys are missing in the .env file");
 }
@@ -39,15 +39,15 @@ const hardhatConfig: HardhatUserConfig = {
     },
     sagaoslocal: {
       // NOTE: this would usually be 8545 but this is set up for hanchond which remaps the ports
-      url: "http://127.0.0.1:57820",
+      url: "http://127.0.0.1:56101",
       chainId: 1234,
-      accounts: process.env.LOCAL_KEYS.split(",")
+      accounts: process.env.LOCAL_KEYS.split(","),
     },
     sagaosstaging: {
       url: "https://sagaevm-54647357-1.jsonrpc.staging-srv.sagarpc.io:443",
       chainId: 54647357,
-      accounts: process.env.TESTNET_KEYS.split(",")
-    }
+      accounts: process.env.TESTNET_KEYS.split(","),
+    },
   },
 };
 
